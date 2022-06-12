6a8da2de8fe7e896a2a21978af67a33b6b2fb7e8
Eugene <ezorin5@yandex.ru>
2022-06-12 15:39:58 +0400

    first

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+const editOpen = document.querySelector('.profile__edit-button');
+
+const inputName = document.querySelector('.popup__name-input');
+
+const inputInfo = document.querySelector('.popup__info-input');
+
+const userName = document.querySelector('.profile__title');
+
+const userInfo = document.querySelector('.profile__subtitle');
+
+const popup = document.querySelector('.popup');
+
+const form = document.querySelector('.popup__container');
+
+function openClose(evt) {
+    if (!popup.classList.contains('popup_opened')) {
+        popup.classList.toggle('popup_opened');
+        inputName.setAttribute('value', `${userName.textContent}`);
+        inputInfo.setAttribute('value', `${userInfo.textContent}`);
+    } else {
+        popup.classList.toggle('popup_opened');
+    }
+}
+
+function clickOutside(evt) {
+    if(evt.target === evt.currentTarget) {
+        openClose();
+    }
+}
+
+function submit(evt) {
+    evt.preventDefault();
+    userName.textContent = `${inputName.value}`;
+    userInfo.textContent = `${inputInfo.value}`;
+    openClose();
+}
+
+editOpen.addEventListener('click', openClose);
+
+form.addEventListener('reset', openClose);
+
+form.addEventListener('submit', submit);
+
+popup.addEventListener('click', clickOutside);
